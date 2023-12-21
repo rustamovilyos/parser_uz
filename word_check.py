4a12fc07ebd397d6bfa7749e777a410bb1513d29
rustamovilyos <rustamov7871@gmail.com>
2023-12-21 12:27:30 +0500

    jsonda dublicate keylarni oʻchirish uchun funksiya yozdim xozircha tekshirib koʻrishga vaqt boʻlmadi

--- word_check.py
+++ word_check.py
@@ -7,27 +7,54 @@ import requests
 
 
 def korrektor_json():
-    url = "http://korrektor.manu.uz/spellcheck"
+    url = "https://korrektor.manu.uz/spellcheck"
     with open('data/correct_data/new_data.csv', 'r') as f:
         reader = csv.reader(f, delimiter='|')
-        for txt_id, row in enumerate(reader):
-            print(f"Checking sentence >> {row[2]}")
-            payload = {'alphabet': 'latin', 'text': str(row[2])}
-            time.sleep(2)
-            headers = {}
-            files = []
-            response = requests.request("POST", url, headers=headers, data=payload, files=files)
-            data = response.json()  # Convert the response to JSON
+        next(reader)  # Skip the header row
 
-            # Store the JSON data in a file
-            with open("data/words.json", "w") as file:
-                json.dump(data, file)
-                time.sleep(1)
+        # Open the JSON file and load the data
+        with open("data/json_data/words_2.json", "r") as file:
+            read_data = json.load(file)
+            # Loop through the rows in the CSV file
+            for txt_id, row in enumerate(reader):
+                if txt_id > 41502:
+                    # Check if the text ID already exists in the JSON file
+                    if str(txt_id) in read_data:
+                        print(f"Text ID {txt_id} already exists in the JSON file")
+                        continue
+                    else:
+                        payload = {'alphabet': 'latin', 'text': str(row[2])}
+                        time.sleep(1)
+                        headers = {}
+                        files = []
+                        response = requests.request("POST", url, headers=headers, data=payload, files=files)
+                        data = response.json()  # Prevent the JSON data from being overwritten
+                        print(f"Checking sentence >> {row[2]} ID-{txt_id}: status code >> {response.json()['code']}")
+                        if response.json()['code'] == 200:
+                            continue
+                        elif response.json()['code'] == 418:
+                            with open("data/json_data/words_2.json", "a") as snd_file:
+                                # Add the text ID to the JSON data
+                                data['text_id'] = txt_id
+                                new_data = {
+                                    str(txt_id): data
+                                }
+
+                                json.dump(new_data, snd_file)
+                                # # file.write('\n')  # Add a newline character for readability
+                                #
+                                time.sleep(1)
+                        else:
+                            print(f"json response Code >> {response.json()['code']}\n")
+                            continue
+                else:
+                    continue
 
 
-korrektor_json()
+# korrektor_json()
 
 
+# tekshirilgan soʻzlarni csv faylga yozish
 def korrektor_check():
     url = "http://korrektor.manu.uz/spellcheck"
 
@@ -135,3 +162,35 @@ def check_csv_file(csv_file):
                     print(f"Corrected sentence: {row[2]}\n")
 
 # check_csv_file('data/correct_data/e-book.csv')
+
+
+# read csv file and write to new csv file with interval 0-20000
+
+
+def delete_words():
+    # check json file and delete keys if similar keys more than one
+    with open('data/json_data/test.json', 'r') as read_json:
+        read_data = json.load(read_json)
+        # print(read_data)
+        all_keys = []
+        for n in range(0, len(read_data)):
+            keys = read_data[n]
+            for key, value in keys.items():
+                # all_keys.append(value['result'].keys())
+                key_words = value['result'].keys()
+                try:
+                    for word in key_words:
+                        if word in all_keys:
+                            print(f"Duplicate word: {word}")
+                            del value['result'][word]
+                        else:
+                            all_keys.append(word)
+                except RuntimeError:
+                    continue
+
+
+# delete_words()
+
+
+
+
